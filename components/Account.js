b6f225c8623813fd6c4df3f53709b236deddd866
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-01-04 17:25:12 +0900

    feat: Add profile photos feature

--- components/Account.js
+++ components/Account.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Avatar from './Avatar';
 
 export default function Account({ session }) {
   const supabase = useSupabaseClient();
@@ -64,6 +65,17 @@ export default function Account({ session }) {
 
   return (
     <div>
+      <div>
+        <Avatar
+          uid={user.id}
+          url={avatar_url}
+          size={150}
+          onUpload={(url) => {
+            setAvatarUrl(url);
+            updateProfile({ username, website, avatar_url: url });
+          }}
+        />
+      </div>
       <div>
         <label htmlFor="email">Email</label>
         <input id="email" type="text" value={session.user.email} disabled />
