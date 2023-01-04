26dbad55653ef29e0dce79d97543e598d4930941
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-01-04 11:31:31 +0900

    feat: Add login functionality

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
 import supabase from '../utils/supabase';
 import NewTodo from '../components/NewTodo';
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Account from '../components/Account';
 
 export default function Home() {
   const [todos, setTodos] = useState([]);
+  const session = useSession();
+  const supabase = useSupabaseClient();
 
   const fetchTodos = async () => {
     const { data } = await supabase.from('todos').select('*');
@@ -17,6 +22,15 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      {!session ? (
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+        />
+      ) : (
+        <Account session={session} />
+      )}
       <NewTodo reload={fetchTodos} />
       {todos.map((todo) => (
         <p key={todo.id}>{todo.title}</p>
